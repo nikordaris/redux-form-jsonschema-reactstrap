d7869845f6d1b508433d55cbc1b90c93d31dd7f4
Nikordaris <nikordaris@gmail.com>
2017-06-28 14:03:23 +0100

    added Card object component. Removed react-jss to fix rendering issue

--- stories/index.js
+++ stories/index.js
@@ -7,7 +7,8 @@ import { reduxForm, reducer as formReducer } from 'redux-form';
 import { Button } from 'reactstrap';
 import SchemaVis from 'react-jsonschema-vis';
 
-import inputFields from '../src';
+import components from '../src';
+import schema from './schema.json';
 
 const rootReducers = combineReducers({ form: formReducer });
 const store = createStore(
@@ -31,6 +32,8 @@ class TestForm extends Component {
   }
 }
 
+const ReduxTestForm = reduxForm({ form: 'test' })(TestForm);
+
 addDecorator(getStory => (
   <Provider store={store}>
     {getStory()}
@@ -40,124 +43,8 @@ addDecorator(getStory => (
 storiesOf(
   'Reactstrap Redux-Form Jsonschema Vis',
   module
-).add('simple form', () => {
-  const schema = {
-    type: 'object',
-    required: ['fullName', 'email', 'password'],
-    properties: {
-      fullName: {
-        id: 'FullName',
-        title: 'Full Name',
-        type: 'string',
-        meta: {
-          vis: {
-            ordinal: 100,
-            editable: true,
-            component: 'TextInputField'
-          }
-        }
-      },
-      email: {
-        id: 'Email',
-        title: 'Email',
-        type: 'string',
-        format: 'email',
-        meta: {
-          vis: {
-            ordinal: 110,
-            editable: true,
-            component: 'EmailInputField'
-          }
-        }
-      },
-      password: {
-        id: 'Password',
-        title: 'Password',
-        type: 'string',
-        meta: {
-          vis: {
-            ordinal: 120,
-            editable: true,
-            component: 'PasswordInputField'
-          }
-        }
-      },
-      dob: {
-        id: 'DOB',
-        title: 'DOB',
-        type: 'string',
-        format: 'date',
-        meta: {
-          vis: {
-            ordinal: 130,
-            editable: true,
-            component: 'DateInputField'
-          }
-        }
-      },
-      numChildren: {
-        id: 'Children',
-        title: '# of Children',
-        type: 'integer',
-        min: 0,
-        meta: {
-          vis: {
-            ordinal: 140,
-            editable: true,
-            component: 'NumberInputField'
-          }
-        }
-      },
-      favColor: {
-        id: 'FavColor',
-        title: 'Favorite Color',
-        type: 'string',
-        meta: {
-          vis: {
-            ordinal: 150,
-            editable: true,
-            component: 'ColorInputField'
-          }
-        }
-      },
-      gender: {
-        id: 'Gender',
-        title: 'Gender',
-        type: 'string',
-        oneOf: [
-          { const: 'Male' },
-          { const: 'Female' },
-          { const: 'Non-binary/ third gender' },
-          { const: 'Prefer not to say' }
-        ],
-        meta: {
-          vis: {
-            ordinal: 160,
-            editable: true,
-            component: 'SelectInputField'
-          }
-        }
-      },
-      comments: {
-        id: 'Comments',
-        title: 'Comments',
-        type: 'string',
-        meta: {
-          vis: {
-            ordinal: 180,
-            editable: true,
-            component: 'TextAreaInputField'
-          }
-        }
-      }
-    }
-  };
-
-  const ReduxTestForm = reduxForm({ form: 'test' })(TestForm);
-
-  return (
-    <ReduxTestForm onSubmit={data => action('form submit')(data)}>
-      <SchemaVis schema={schema} components={inputFields} />
-    </ReduxTestForm>
-  );
-});
+).add('simple form', () => (
+  <ReduxTestForm onSubmit={data => action('form submit')(data)}>
+    <SchemaVis schema={schema} components={components} />
+  </ReduxTestForm>
+));
