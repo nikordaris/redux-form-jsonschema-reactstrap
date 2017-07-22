b40357f81854d7a7cd74ab0ea78d09f64e8dfc8b
Nikordaris <nikordaris@gmail.com>
2017-07-22 15:24:55 +0100

    added varied array components

--- src/components/ArrayFields.test.js
+++ src/components/ArrayFields.test.js
@@ -6,7 +6,12 @@ import { createStore, combineReducers } from 'redux';
 import { Button } from 'reactstrap';
 import toJson from 'enzyme-to-json';
 
-import { UniformedArrayCard, UniformedArrayInline } from '../index';
+import {
+  UniformedArrayCard,
+  UniformedArrayInline,
+  VariedArrayCard,
+  VariedArrayInline
+} from '../index';
 import { matchSnapshotWithReduxForm } from '../testUtils';
 import components from './index';
 
@@ -69,7 +74,7 @@ describe('Render ArrayFields', () => {
     )
   );
 
-  it('should handle add item and remove item', () => {
+  it('should handle add item and remove Uniformed item', () => {
     const rootReducers = combineReducers({ form: formReducer });
     const store = createStore(rootReducers);
     const WrappedComponent = reduxForm({ form: 'MyForm' })(UniformedArrayCard);
@@ -100,6 +105,300 @@ describe('Render ArrayFields', () => {
         />
       </Provider>
     );
+    wrapper.find('#addItemBtn').simulate('click');
+    expect(toJson(wrapper)).toMatchSnapshot();
+    wrapper.find('#removeItemBtn').at(0).simulate('click');
+    expect(toJson(wrapper)).toMatchSnapshot();
+    wrapper.unmount();
+  });
+
+  it(
+    'Should render VariedArrayCard',
+    matchSnapshotWithReduxForm(
+      {
+        schemaVis: {
+          prefix: 'meta.vis',
+          schema: {
+            id: 'VariedArrayCard',
+            title: 'Varied Array Card',
+            type: 'array',
+            items: {
+              anyOf: [
+                {
+                  id: 'Bar',
+                  title: 'Bar',
+                  type: 'object',
+                  meta: {
+                    vis: {
+                      ordinal: 1,
+                      component: 'CardItem'
+                    }
+                  },
+                  properties: {
+                    name: {
+                      title: 'Name',
+                      it: 'Name',
+                      type: 'string',
+                      meta: {
+                        vis: {
+                          ordinal: 1,
+                          component: 'Text'
+                        }
+                      }
+                    },
+                    bar: {
+                      id: 'Bar',
+                      title: 'Bar',
+                      type: 'string',
+                      meta: {
+                        vis: {
+                          ordinal: 2,
+                          component: 'Text'
+                        }
+                      }
+                    }
+                  }
+                },
+                {
+                  id: 'Baz',
+                  title: 'Baz',
+                  type: 'object',
+                  meta: {
+                    vis: {
+                      ordinal: 1,
+                      component: 'CardItem'
+                    }
+                  },
+                  properties: {
+                    name: {
+                      title: 'Name',
+                      it: 'Name',
+                      type: 'string',
+                      meta: {
+                        vis: {
+                          ordinal: 1,
+                          component: 'Text'
+                        }
+                      }
+                    },
+                    baz: {
+                      id: 'Baz',
+                      title: 'Baz',
+                      type: 'number',
+                      meta: {
+                        vis: {
+                          ordinal: 2,
+                          component: 'Number'
+                        }
+                      }
+                    }
+                  }
+                }
+              ]
+            }
+          },
+          components
+        },
+        name: 'Foo',
+        form: 'MyForm'
+      },
+      VariedArrayCard
+    )
+  );
+
+  it(
+    'Should render VariedArrayInline',
+    matchSnapshotWithReduxForm(
+      {
+        schemaVis: {
+          prefix: 'meta.vis',
+          schema: {
+            id: 'VariedArrayInline',
+            title: 'Varied Array Inline',
+            type: 'array',
+            items: {
+              anyOf: [
+                {
+                  id: 'Bar',
+                  title: 'Bar',
+                  type: 'object',
+                  meta: {
+                    vis: {
+                      ordinal: 1,
+                      component: 'CardItem'
+                    }
+                  },
+                  properties: {
+                    name: {
+                      title: 'Name',
+                      it: 'Name',
+                      type: 'string',
+                      meta: {
+                        vis: {
+                          ordinal: 1,
+                          component: 'Text'
+                        }
+                      }
+                    },
+                    bar: {
+                      id: 'Bar',
+                      title: 'Bar',
+                      type: 'string',
+                      meta: {
+                        vis: {
+                          ordinal: 2,
+                          component: 'Text'
+                        }
+                      }
+                    }
+                  }
+                },
+                {
+                  id: 'Baz',
+                  title: 'Baz',
+                  type: 'object',
+                  meta: {
+                    vis: {
+                      ordinal: 1,
+                      component: 'CardItem'
+                    }
+                  },
+                  properties: {
+                    name: {
+                      title: 'Name',
+                      it: 'Name',
+                      type: 'string',
+                      meta: {
+                        vis: {
+                          ordinal: 1,
+                          component: 'Text'
+                        }
+                      }
+                    },
+                    baz: {
+                      id: 'Baz',
+                      title: 'Baz',
+                      type: 'number',
+                      meta: {
+                        vis: {
+                          ordinal: 2,
+                          component: 'Number'
+                        }
+                      }
+                    }
+                  }
+                }
+              ]
+            }
+          },
+          components
+        },
+        name: 'Foo',
+        form: 'MyForm'
+      },
+      VariedArrayInline
+    )
+  );
+  it('should handle add item and remove Varied item', () => {
+    const rootReducers = combineReducers({ form: formReducer });
+    const store = createStore(rootReducers);
+    const WrappedComponent = reduxForm({ form: 'MyForm' })(VariedArrayCard);
+    const schema = {
+      id: 'VariedArrayCard',
+      title: 'Varied Array Card',
+      type: 'array',
+      items: {
+        anyOf: [
+          {
+            id: 'Bar',
+            title: 'Bar',
+            type: 'object',
+            meta: {
+              vis: {
+                ordinal: 1,
+                component: 'CardItem'
+              }
+            },
+            properties: {
+              name: {
+                title: 'Name',
+                it: 'Name',
+                type: 'string',
+                meta: {
+                  vis: {
+                    ordinal: 1,
+                    component: 'Text'
+                  }
+                }
+              },
+              bar: {
+                id: 'Bar',
+                title: 'Bar',
+                type: 'string',
+                meta: {
+                  vis: {
+                    ordinal: 2,
+                    component: 'Text'
+                  }
+                }
+              }
+            }
+          },
+          {
+            id: 'Baz',
+            title: 'Baz',
+            type: 'object',
+            meta: {
+              vis: {
+                ordinal: 1,
+                component: 'CardItem'
+              }
+            },
+            properties: {
+              name: {
+                title: 'Name',
+                it: 'Name',
+                type: 'string',
+                meta: {
+                  vis: {
+                    ordinal: 1,
+                    component: 'Text'
+                  }
+                }
+              },
+              baz: {
+                id: 'Baz',
+                title: 'Baz',
+                type: 'number',
+                meta: {
+                  vis: {
+                    ordinal: 2,
+                    component: 'Number'
+                  }
+                }
+              }
+            }
+          }
+        ]
+      }
+    };
+    const wrapper = mount(
+      <Provider store={store}>
+        <WrappedComponent
+          schemaVis={{
+            components,
+            prefix: 'meta.vis',
+            schema
+          }}
+          name="foobar"
+          form="MyForm"
+        />
+      </Provider>
+    );
+    wrapper
+      .find('#selectItem')
+      .simulate('change', { target: { value: 'Bar' } });
+    expect(toJson(wrapper)).toMatchSnapshot();
     wrapper.find('#addItemBtn').simulate('click');
     expect(toJson(wrapper)).toMatchSnapshot();
     wrapper.find('#removeItemBtn').at(0).simulate('click');
